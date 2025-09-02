ee915a324b516e8efdaeeef051ad829ac9b0beb5
themightymessage3-maker <themightymessage3@gmail.com>
2025-09-02 23:36:40 +0100

    Add files via upload

--- user.js
+++ user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  status: { type: String, enum: ['active', 'inactive'], default: 'active' },
   billingInfo: {
     name: String,
     address: String,
